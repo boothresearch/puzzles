d3c01bcfb86ee0707e343a98ee3f710ecbdf500b
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 18:44:28 +0000

    fixed darts

--- darts/darts.py
+++ darts/darts.py
@@ -2,13 +2,13 @@ x = int(input('Please enter the x coordinate:'))
 y = int(input('Please enter the y coordinate:'))
 def score(x, y):
     if x**2+y**2>100:
-        print("The score is 0")
+        return 0 
     elif x**2+y**2<=100 and x**2+y**2>25:
-        print("The score is 1")
+        return 1 
     elif x**2+y**2<=25 and x**2+y**2>1:
-        print("The score is 5")   
+        return 5 
     elif x**2+y**2>=0 and x**2+y**2<=1:
-        print("The score is 10")  
+        return 10 
     else:
         print("Error")
-score(x,y)
+print("The score is: ", score(x,y))
