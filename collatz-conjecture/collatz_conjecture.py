70cfa4ba4fd059d2ccf56deb252994b54d5c7b4a
Milica Popovic <milicapopovic@uchicago.edu>
2019-09-18 19:50:38 +0000

    Polished the code

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,19 @@
 def steps(number):
-    pass
+    if isinstance(number, int):
+        if number > 0:
+            pass
+        else: 
+            raise ValueError('The number should be an integer and greater then 0')
+    else:
+        raise TypeError('The number should be of type integer')
+        
+    n = number
+
+    step = 0
+    while n !=1:
+        if n % 2 == 0:
+            n = n/2        
+        else:
+            n = 3 * n+1
+        step +=1
+    return step
