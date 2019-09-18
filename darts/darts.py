8c18b4428132e1d5730a895c6e158b1f06b29f15
Xuyang Xia <xuyangxiaxyx@gmail.com>
2019-09-18 15:17:32 +0000

    darts by Xuyang

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,12 @@
 def score(x, y):
-    pass
+    dis = (x*x+y*y)**0.5;
+    if dis<=10 and dis>5:
+        score = 1
+    elif dis<=5 and dis>1:
+        score = 5
+    elif dis <=1: 
+        score = 10
+    else:
+        score = 0
+    print(score)
+        
--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,10 @@
 def leap_year(year):
-    pass
+    if year%4==0:
+        if year%400==0:
+         print("leap year")
+        elif year%100==0:
+         print("not leap year")
+        else:
+            print("leap year")
+    print("not leap year")
+            
