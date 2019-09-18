858bc7de697d49bfdc3b9a04d6e0f5ba920db59b
Xuyang Xia <xuyangxiaxyx@gmail.com>
2019-09-18 16:14:38 +0000

    anagram by Xuyang

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,6 @@
 def find_anagrams(word, candidates):
-    pass
+    list = []
+    for i in candidates:
+        if ''.join(sorted(i))==''.join(sorted(word)):
+            list.append(i)
+    print(list)
