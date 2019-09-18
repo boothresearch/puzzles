2c9915a639c294499c2f5781c7930655ebf19db1
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 18:16:25 +0000

    Some edits

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
-def is_isogram(string):
-    pass
+def is_isogram(word):
+    if isinstance(word,str):
+        w = word.lower() # assuming you want a case in-sensitive search
+        return word, len(w) == len(set(w)) if w else False
+    else:
+        raise TypeError('Argument should be a string')
+            
