37f579e769ae237b1742740b8f27b8ef054c5eac
Laurence O'Brien <laurence.obrien@chicagobooth.edu>
2019-09-18 19:37:36 +0000

    Edited leap

--- leap/leap.py
+++ leap/leap.py
@@ -1,11 +1,19 @@
 def leap_year(year):
+    
+    #Check if year is divisible by 400, then leap
     if year % 400 == 0:
-        return(str(year) + " is a leap year")
+        return(True)
+    
+    #If not, check if year is divisible by 100, then false
     elif year % 100 == 0:
-        return(str(year) + " is not a leap year")
+        return(False)
+    
+    #If not, check if year is divisible by 4, then true
     elif year % 4 == 0:
-        return(str(year) + " is a leap year")
+        return(True)
+    
+    #Else, year is not a leap year
     else:
-        return(str(year) + " is not a leap year")
+        return(False)
 
 
