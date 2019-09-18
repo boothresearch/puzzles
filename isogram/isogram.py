c5640900e215b39a6c63d65b584765a0d89a08d2
JIN DENG <jin.deng@chicagobooth.edu>
2019-09-18 15:36:13 +0000

    trial

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,14 @@
+from collections import Counter 
+
 def is_isogram(string):
-    pass
+    input = string
+    # count the number of ocurrence of each element 
+    frequency = Counter(input)
+    # if no character appear more than once - isogram
+    print(str(frequency))
+    # if a character appears more than once - and they are not space or hypens - not isogram
+    # 
+    #   if they are space or hypens - isogram
+    #   
+
+is_isogram()    
