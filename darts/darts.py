099ae2fab592d4717febec1e5490dfebd9c40980
Pranav Garg <pranav.garg@chicagobooth.edu>
2019-09-18 15:57:24 +0000

    I finished darts.

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
+import math
+
 def score(x, y):
-    pass
+    d=math.sqrt(x*x + y*y)
+    if d<=10 and d>5:
+        return("Your score is 1")
+    elif d<=5 and d>1:
+        return("Your score is 5")
+    elif d<=1:
+        return("Your score is 10")
+    else:
+        return("You missed the target")
+    
+print(score(1, 3))
