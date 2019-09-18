4ebed24efbef50844b39c35989f0d5b5f19514c6
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 18:33:22 +0000

    deleted duplicate codes in scrabble_score

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,45 +1,3 @@
-one=["A","E", "I", "O", "U", "L", "N", "R", "S", "T"]
-two=["D", "G"]
-three=["B", "C", "M", "P"]
-four=["F", "H", "V", "W", "Y"]
-five=["K"]
-six=["J","X"]
-seven=["Q", "Z"]
-count=0
-points=0
-theword=list(input("Please input a word: "))
-
-def score(word, count, points):
-    for i in word: 
-        if i in one:
-            points+=1
-            count = count + 1
-        elif i in two:
-            points+=2
-            count = count + 1
-        elif i in three:
-            points+=3
-            count = count + 1
-        elif i in four:
-            points+=4
-            count = count + 1
-        elif i in five:
-            points+=5
-            count = count + 1 
-        elif i in six:
-            points+=6
-            count = count + 1 
-        elif i in seven:
-            points+=7
-            count = count + 1 
-        else:
-            print("Error")
-    print("The score is: " +str(points))           
-
-score(theword, count, points)
-
-
-
 
 score = {"a": 1 , "b": 3 , "c": 3 , "d": 2 ,
          "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
@@ -49,7 +7,7 @@ score = {"a": 1 , "b": 3 , "c": 3 , "d": 2 ,
          "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
          "y": 4 , "z": 10}
 
-def word_score(word):
+def score(word):
     total = 0;
     for i in word:
         i = i.lower(); 
@@ -58,5 +16,5 @@ def word_score(word):
 
 your_word = input("Please input a word: ");
 
-print(word_score(your_word))
+print(score(your_word))
 
