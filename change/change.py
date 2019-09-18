8201b33b8d19e7684b33721c427af68c161e7e77
Alejandro Herrera <alejandro.herreracaicedo@chicagobooth.edu>
2019-09-18 16:47:18 +0000

    Change

--- change/change.py
+++ change/change.py
@@ -1,2 +1,31 @@
+#Define minimum number of coins 
+#No mather how the coin array is arranged
 def find_fewest_coins(coins, target):
-    pass
+    
+    #Create variable to storage results 
+    output = [None]*len(coins)
+    
+    #Define change left
+    changeLeft = target
+    
+    while changeLeft>0:
+        #Number of coins required for that ammount
+        neededCoins=[changeLeft//i for i in coins]
+        
+        #Only if number of coins is positive
+        tmp = [i for i in neededCoins if i>0]
+        
+        #Which is the biggest coin
+        noBiggestCoin = min(tmp)
+        
+        #Retrieve coordinate
+        biggestCoinIndex = neededCoins.index(noBiggestCoin)
+        
+        #Ammount paid in biggest coin
+        Ammount = noBiggestCoin * coins[biggestCoinIndex]
+        output[biggestCoinIndex] = noBiggestCoin
+        
+        #Whats left
+        changeLeft = changeLeft-Ammount
+        
+    return(output)
