09e853b34eee6f7fb0d739f65aa6ba370ababc20
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 19:59:21 +0000

    Nothing

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,12 @@
 def rotate(text, key):
-    
+    def caesar(plainText, shift): 
+  cipherText = ""
+  for ch in plainText:
+    if ch.isalpha():
+      stayInAlphabet = ord(ch) + shift 
+      if stayInAlphabet > ord('z'):
+        stayInAlphabet -= 26
+      finalLetter = chr(stayInAlphabet)
+      cipherText += finalLetter
+  print "Your ciphertext is: ", cipherText
+  return cipherText
