d2279d87c8ba30b328affafcaf178ce12e964626
Byeongchan An <byeongchan.an@chicagobooth.edu>
2019-09-18 16:49:45 +0000

    anagram done

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,6 +1,22 @@
 def find_anagrams(word, candidates):
-    print(sorted(word))
-    print(sorted(candidates))
+    results = []
+    for i in range(len(candidates)):
+        word_sorted = sorted(word)
+        cand_sorted = sorted(candidates[i])
+        if word_sorted == cand_sorted:
+            results.append(candidates[i])
+#            print(candidates[i])
+#        else:
+#            print("No matched word!")
+    
+    if len(results) == 0:
+        print("There is no match")
+    else:
+        print("Matches word(s)...:")
+        for i in range(len(results)):
+            print(results[i])
 
 
-find_anagrams("hello", "lleho")
+
+find_anagrams("hello", ["lleho", "howdy", "pie", "lehol"])
+find_anagrams("listen", ["enlists", "google", "inlets", "banana"])
