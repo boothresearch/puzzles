8487951a48cd737b9694352db8c6bac79661f430
Zichen Zhao <zichen.zhao@chicagobooth.edu>
2019-09-18 15:22:10 +0000

    Darts finished

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
+x = int(input('Please enter the x coordinate:'))
+y = int(input('Please enter the y coordinate:'))
 def score(x, y):
-    pass
+    if x**2+y**2>100:
+        print("The score is 0")
+    elif x**2+y**2<=100 and x**2+y**2>25:
+        print("The score is 1")
+    elif x**2+y**2<=25 and x**2+y**2>1:
+        print("The score is 5")   
+    elif x**2+y**2>=0 and x**2+y**2<=1:
+        print("The score is 10")  
+    else:
+        print("Error")
+score(x,y)
--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,4 @@
 def leap_year(year):
+    if (year%4==0&year%year%100!=0):
+     print(year "is a leap year")
     pass
