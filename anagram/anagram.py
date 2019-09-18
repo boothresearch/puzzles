9224490ddc67e2fc41196c9f939a4dfb86d787fc
Ginha Kim <ginha.kim@chicagobooth.edu>
2019-09-18 16:57:29 +0000

    anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,31 @@
 def find_anagrams(word, candidates):
-    pass
+    
+    anagram = []
+    
+    word_letters = []
+    
+    while len(word_letters)<len(word):
+        for i in list(word):
+            if i not in word_letters:
+                word_letters.append(i)
+    
+    word_letters = sorted(word_letters)
+    
+    for i in candidates:
+        
+        i_letters= []
+        
+        while len(i_letters)<len(i):
+            for j in list(i):
+                if j not in i_letters:
+                    i_letters.append(j)
+        
+        i_letters = sorted(i_letters)
+        
+        done = (i_letters == word_letters)
+        
+        if done == True:
+            
+            anagram.append(i)
+    
+    return anagram
