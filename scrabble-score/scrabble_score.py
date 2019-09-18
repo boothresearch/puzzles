db68204b8029d6392c5f02ae81195abd82c79773
Milica Popovic <milicapopovic@uchicago.edu>
2019-09-18 16:09:49 +0000

    Scrabble score done

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,26 @@
 def score(word):
-    pass
+    word = word.lower()
+    group1 = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
+    group2 = ['d', 'g']
+    group3 = ['b', 'c', 'm', 'p']
+    group4 = ['f', 'h', 'v', 'w', 'y']
+    group5 = ['k']
+    group8 = ['j', 'x']
+    group10 = ['q', 'z']
+    score = 0
+    for letter in word:
+        if letter in group1:
+            score += 1
+        elif letter in group2:
+            score += 2
+        elif letter in group3:
+            score += 3
+        elif letter in group4:
+            score += 4
+        elif letter in group5:
+            score += 5
+        elif letter in group8:
+            score += 8
+        elif letter in group10:
+            score += 10
+    return score
