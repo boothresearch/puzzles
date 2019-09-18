7cb087f8ac1e6696be34df69d93388a01c83b8fe
Xuyang Xia <xuyangxiaxyx@gmail.com>
2019-09-18 19:45:57 +0000

    add case sensitive

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,6 +1,6 @@
 def find_anagrams(word, candidates):
     list = []
     for i in candidates:
-        if ''.join(sorted(i))==''.join(sorted(word)):
+        if ''.join(sorted(i)).lower()==''.join(sorted(word)).lower():
             list.append(i)
     print(list)
