e4686d920c3536bedb7c414f6812dd3529be4756
Valerii Baidin <vbaidin@uchicago.edu>
2019-09-18 16:16:26 +0000

    LEAP final

--- leap/leap.py
+++ leap/leap.py
@@ -1,7 +1,11 @@
-print("begin")
-year=1999
-remainder new !!!
 def leap_year(year):
-    print("asdf")
-    #return(year % 4)
+    
+    div_4    = (year % 4)   ==0
+    div_100  = (year % 100) ==0
+    div_400  = (year % 400) ==0
+    
+    if div_4 and (not div_100 or div_400): 
+        print("the year " + str(year) + " is leap")
+    else:
+        print("the year " + str(year) + " is not leap")
 
