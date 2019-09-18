4665b119e8bda6ecdf4ba45f4705185fd834d5c0
aherrerac10 <40305468+aherrerac10@users.noreply.github.com>
2019-09-18 13:12:11 -0500

    Update phone_number.py

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,9 +1,11 @@
 import re 
 def Phone(phone):
+    #Turn into string if it is not already
     phone = str(phone)
+    #Remove everything but numbers
     phone = re.sub('[^0-9]','', phone)
+    #Keep last 11 numbers
     phone = phone[len(phone)-10:len(phone)]
-    
+    #Print
     print(phone)
-    #def __init__(self, phone_number):
-    #    pass
+
