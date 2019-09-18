7c94d9a68e50550a32e49aa0cd7d93641b79fd3b
Maddalena Grignani <maddalena.grignani@chicagobooth.edu>
2019-09-18 18:23:23 +0000

    finished darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,21 @@
 def score(x, y):
-    pass
+    # calculate Euclidean distance
+    d = x**2 + y**2
+    
+    #defining scores
+    if (d > 100):
+        score=0
+    
+    elif (d<=100) & (d>25):
+        score = 1 
+        
+    elif (d<= 25) & (d>1):
+        score =5
+        
+    elif (d <=1):
+        score =10 
+
+    #return function
+    return("your score is " + str(score))
+
+#print(score(30,2))
