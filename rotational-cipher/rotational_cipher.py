8f2f857a8c91d4260b98044ac758b70c57edfc0f
Sam Wallach Hanson <sam.wallach.hanson@chicagobooth.edu>
2019-09-18 18:53:58 +0000

    rotate

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,11 @@
+from string import *
 def rotate(text, key):
-    pass
+    cyphertext = ""
+    text = str.lower(text)
+    for letter in text:
+        letterint = ord(letter) - 96
+        if letterint > 0 and letterint < 27:
+            cyphertext = cyphertext + chr(((letterint + key)%26)+96)
+        else:
+            cyphertext = cyphertext + letter
+    print(cyphertext)
