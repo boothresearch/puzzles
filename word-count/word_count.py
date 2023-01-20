7c3b53ea3454b59103cc9968703a9f2db4306d59
Zhen Yuan <vinsonyuan96@gmail.com>
2023-01-20 17:00:17 +0000

    complete word_count.py

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,13 @@
 def count_words(sentence):
-    pass
+    import re
+    word_dic = {}
+    word_list = re.findall(r"[0-9A-Za-z']+", sentence)
+    print(word_list)
+    for word in word_list:
+        word = word.lower().strip("'")
+        if word not in word_dic:
+            word_dic[word] = 1
+        else:
+            word_dic[word] += 1
+
+    return word_dic
