9404749cb90d52e6f0fa8f180847fb4d38c71b0d
goldma99 <goldm@uchicago.edu>
2023-01-20 17:04:27 +0000

    collatz steps fn

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,18 @@
+
 def steps(number):
-    pass
+    
+    n_steps = 0
+    
+    if number <= 0:
+        return "Only strictly positive integers allowed!"
+    elif number == 1:
+        return n_steps
+    else:
+        while (number != 1):
+            if number % 2 == 0:
+                number = number / 2
+                n_steps += 1
+            else:
+                number = 3*number + 1
+                n_steps += 1
+    return n_steps
