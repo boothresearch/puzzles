005ec8804439c03fea4d0106a0ab9d054c9defec
avshah1 <anand.v.shah@hotmail.com>
2023-01-20 16:41:02 +0000

    Complete collatz_conjecture.py

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,14 @@
-def steps(number):
-    pass
+counter = 0
+def steps(number, counter = 0):
+    if number < 1:
+        raise ValueError('number should be positive')
+    elif number % 2 == 0:
+        number = number / 2
+        counter += 1
+        return steps(number, counter)
+    elif number == 1:
+        return counter
+    else:
+        number = 3*number + 1
+        counter += 1
+        return steps(number, counter)
