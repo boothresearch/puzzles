578950ed48fe6a614232246d4660a00565d8aab5
ruoyuc99 <rchen4@uchicago.edu>
2023-01-20 16:10:55 +0000

    completed leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,11 @@
 def leap_year(year):
-    pass
+    if year % 4 !=0:
+        return(False)
+    else:
+        if year % 400 == 0:
+            return(True)
+        elif year % 100 == 0:
+            return(False)
+        else:
+            return(True)
+
