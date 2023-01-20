ba40e7fae7c0fc52d5df41f79e730686754ca703
Jieyi-Chen-98 <jieyi@uchicago.edu>
2023-01-20 16:15:31 +0000

    Complete leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,5 +1,11 @@
 def leap_year(year):
-    if year % 4 == 0:
+    if year % 400 == 0:
+        return True
+    elif year % 200 == 0:
+        return False
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
         return True
     else:
         return False
