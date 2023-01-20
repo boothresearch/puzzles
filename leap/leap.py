330906f037d8114a10c2435d8ba629b26318f0b2
anhtran1405 <at3981@nyu.edu>
2023-01-20 16:14:42 +0000

    Complete leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,8 @@
 def leap_year(year):
-    pass
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    else:
+        return False
+#print(leap_year(1900))
