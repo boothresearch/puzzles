80b3c264262f514b515bb1b13f251203cdff5f5f
goldma99 <goldm@uchicago.edu>
2023-01-20 16:14:12 +0000

    leap fn

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,9 @@
 def leap_year(year):
-    pass
+    if year % 4 == 0:
+        return True
+    elif year % 4 == 0 & year % 100 == 0:
+        return False
+    elif year % 4 == 0 & year % 100 == 0 & year % 400 == 0:
+        return True
+    else:
+        return False
