50eae39b224447c7e0c4f13364317238e481b70a
xmj1112 <xiaominju@outlook.com>
2024-09-24 15:53:27 +0000

    Complete darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,9 @@
 def score(x, y):
-    pass
+    if (x**2 + y**2) <= 1:
+        return 10
+    elif (x**2 + y**2) <= 25:
+        return 5
+    elif (x**2 + y**2) <= 100:
+        return 1
+    else:
+        return 0
