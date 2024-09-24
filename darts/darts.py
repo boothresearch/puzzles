7e11803d78fcc0430e0d29d8cea347df433313fd
Shuyang Ma <86862962+Peterhungchien@users.noreply.github.com>
2024-09-24 15:05:58 +0000

    Complete darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,10 @@
 def score(x, y):
-    pass
+    dist = x**2 + y**2
+    if dist <= 1:
+        return 10
+    elif dist <=25:
+        return 5
+    elif dist <=100:
+        return 1
+    else:
+        return 0
