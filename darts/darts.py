6b26d2fa1f7567b227a7fda57be8226d895deb3c
mwranderson <mary.w.rumsey@gmail.com>
2024-09-24 16:08:16 +0000

    Complte leap.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
+import math
+
 def score(x, y):
-    pass
+    distance = math.sqrt(x**2 + y**2)
+    if distance <= 1 :
+        return 10
+    elif distance <= 5 :
+        return 5
+    elif distance <= 10 :
+        return 1
+    else:
+        return 0 
+
+print(score(3,4))
