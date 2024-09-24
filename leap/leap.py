c5fd9dfaae4c507365670060f3696f6d3dd93e99
sanghwaahn <sanghwa@uchicago.edu>
2024-09-24 15:11:28 +0000

    Complet leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,23 @@
 def leap_year(year):
-    pass
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+print(leap_year(1999))
+    #pass
+# ctrl-j 
+# cd leap
+# python3 leap.py
+# python3 -i leap.py
+# 
+
+
+
+## My Exercise
+# exit() or quit()
+# q to return
