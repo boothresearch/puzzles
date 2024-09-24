5143b44b44f6721026adaedfb6d2e870f64ecfa9
xmj1112 <xiaominju@outlook.com>
2024-09-24 15:59:32 +0000

    Complete collatz-conjecture.py

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    count = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = number * 3 + 1
+        count += 1
+    return count
