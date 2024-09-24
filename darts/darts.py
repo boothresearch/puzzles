6651b6410b006c54f1219fe8746de1daa56d7acb
yijing02 <Yijing.Ren@chicagobooth.edu>
2024-09-24 15:52:43 +0000

    Complete darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,15 @@
+OUTER_RADIUS = 10
+MIDDLE_RADIUS = 5
+INNER_RADIUS = 1
+
 def score(x, y):
-    pass
+    distance_squared = x**2 + y**2
+
+    if distance_squared <= INNER_RADIUS**2:
+        return 10
+    elif distance_squared <= MIDDLE_RADIUS**2:
+        return 5
+    elif distance_squared <= OUTER_RADIUS**2:
+        return 1
+    else:
+        return 0
