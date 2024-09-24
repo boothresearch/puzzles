ba660140f0f9f27a986e79e7cc48f8b6ebe1f83f
wwchicago <jinrui.wang.2001@gmail.com>
2024-09-24 14:51:38 +0000

    Complete leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,10 @@
 def leap_year(year):
-    pass
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+#print(leap_year(1999))
