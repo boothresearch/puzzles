1f6052bca53ab369dac15fa7a0557944d2d75ab7
BrandonMathu <58142665+BrandonMathu@users.noreply.github.com>
2024-09-24 15:00:51 +0000

    leap_year

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,15 @@
 def leap_year(year):
-    pass
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+#copied sol in notes
+#   on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400      
+
+# print(leap_year(2000))
